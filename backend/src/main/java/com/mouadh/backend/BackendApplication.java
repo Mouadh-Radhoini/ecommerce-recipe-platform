9ebe1655e3c7190d67e6686121bf4096be197534
Mouadh Radhoini <rdmouadh@gmail.com>
2026-01-12 15:35:53 +0100

    fix: align base package and move BackendApplication

--- backend/src/main/java/com/mouadh/backend/BackendApplication.java
+++ backend/src/main/java/com/mouadh/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package mouadh.example.backend;
+package com.mouadh.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
